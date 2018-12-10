b0e8aa9287a942a6cdc4c4130b18e6924c690591
jvarghe <vcs.inbox@outlook.com>
2018-12-10 16:47:45 -0500

    Fleshed out the 'Node' class.

---
+++ LinkedList/src/com/jvarghe/Node.java
@@ -0,0 +1,63 @@
+/* NODE CLASS
+ * 
+ * This class models an element in a Linked List. An element is called a Node. A node has
+ * two parts, the Data value for this element, and the address to the next node. 
+ */
+
+
+package com.jvarghe;
+
+public class Node<T extends Comparable<T>> 
+{
+    // PROPERTIES:
+    // The Value of the Element Stored in the Current Node.
+    private T element; 
+    
+    // The Address of the Next Node.
+    private Node<T> nextNodeAddress;
+    
+    
+    // METHODS:
+    // Node Constructor
+    public Node(T value)
+    {
+        this.element = value;
+        setNextNode(null);
+    }
+    
+    
+    // Element Getter: Return the Current Node's Data Value. 
+    public T getElement()
+    {
+        return element;
+    }
+    
+    
+    // Element Setter: Set the Current Node's Data Value.
+    public void setElement(T value)
+    {
+        this.element = value; 
+    }
+    
+    // Next Node Getter: Get the Address of the Next Node. 
+    public Node<T> getNextNode()
+    {
+        return nextNodeAddress;
+    }
+    
+    
+    // Next Node Setter: Set the Address of the Next Node. 
+    public void setNextNode(Node<T> nextNode)
+    {
+        this.nextNodeAddress = nextNode;
+    }
+    
+    
+    // Overrides Java's String method in the base Object class to print Node class's 
+    // values to screen. 
+    @Override
+    public String toString()
+    {
+        return String.valueOf(this.element);
+    }
+}
