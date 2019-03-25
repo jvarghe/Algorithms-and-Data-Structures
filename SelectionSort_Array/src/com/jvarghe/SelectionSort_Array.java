1ae761a63da30068f8050cac6905e66d948214f3
jvarghe <vcs.inbox@outlook.com>
2019-03-25 13:46:45 -0400

    Edited the SELECTION SORT notes in 'SelectionSort_Array.java'.

--- SelectionSort_Array/src/com/jvarghe/SelectionSort_Array.java
+++ SelectionSort_Array/src/com/jvarghe/SelectionSort_Array.java
@@ -1,22 +1,30 @@
 /* SELECTION SORT
  * 
- * Selection Sort is an inefficient sorting algorithm. It sorts the contents of a list in either
- * increasing or decreasing order. For the sake of explanation, I'll use increasing order:
+ * Selection Sort is an inefficient sorting algorithm. It sorts the contents of a list in 
+ * either increasing or decreasing order. For the sake of explanation, I'll use 
+ * increasing order. Selection Sort will compare the element values of a list, and order 
+ * them from the lowest value to highest.
  * 
- * Selection sort selects the first element of a list and compares it to all subsequent elements,
- * looking for the the lowest value. When it finds the lowest value, it swaps this value into the 
- * first index position. Ignoring the sorted element in the first index, the algorithm moves to the 
- * second element in the list and compares it to all latter elements, looking for the second lowest
- * value. Then, selection sort does the same thing with the third place element and so on. Halfway 
- * through this process, the algorithm will have separated the list into two portions: the first 
- * segment of the list, which is now sorted in ascending order, and second segment, which is still 
- * to be sorted. Selection sort continues this process until the entire list has been sorted.
+ * When you run Selection Sort on a list, it selects the first element of a list and 
+ * compares it to all subsequent elements, looking for the the lowest value. When the 
+ * lowest value in the list is found, if it is the first element, nothing is changed. If
+ * the lowest value is not the first element, it gets swapped with the value in the first 
+ * index position. 
  * 
- * Selection sort is an inefficient algorithm, suited only for small data sets. Its runtime is 
- * O(n^2). Why does it run in exponential time? Selection sort has to check n elements * (n - 1)
- * elements * (n - 2) elements ... 3, 2, 1. On average, this means that selection sort has to check
- * (1/2n) * n elements. As Big-O Notation is concerned more with orders of magnitude than multiples, 
- * this is rounded up to n * n.
+ * Then, the algorithm moves on to the second element in the list and compares it to all 
+ * latter elements, looking for the second lowest value. After finding and swapping these 
+ * values, Selection Sort does the same thing with the third place element and so on. 
+ * Here is a snapshot of what the list will look like halfway through the sorting process: 
+ * the algorithm will have separated the list into two portions. The first segment of the 
+ * list, which is now sorted in ascending order, and second segment, which is still 
+ * to be sorted. Selection sort will continue the sorting process until the entire list 
+ * has been sorted.
+ * 
+ * Selection sort is an inefficient algorithm, suited only for small data sets. Its 
+ * runtime is O(n^2). Why does it run in exponential time? Selection sort has to check n 
+ * elements * (n - 1) * elements * (n - 2) elements ... 3, 2, 1. On average, this means 
+ * that selection sort has to check (1/2n) * n elements. As Big-O Notation is concerned 
+ * more with orders of magnitude than multiples, this is rounded up to n * n.
  */
 
 
