8de8f4b1451732b0bdca18f184cb0dbc38b6a04b
jvarghe <vcs.inbox@outlook.com>
2019-04-01 19:22:05 -0400

    Added the 'Divide-and-Conquer Strategy' section in 'QuickSort_Array.java'. Also, made other edits to comment section.

--- QuickSort_Array/src/com/jvarghe/QuickSort_Array.java
+++ QuickSort_Array/src/com/jvarghe/QuickSort_Array.java
@@ -1,18 +1,66 @@
-/* QUICKSORT
+/* QUICK SORT
  *
- * The Quicksort (QS) algorithm is a moderately fast one, but one that could take 
- * exponential time in the worst case. Nevertheless, it is one of the most commonly used 
- * sorting algorithms.
- *
- * Quicksort is a recursive algorithm that uses the Divide-and-Conquer strategy to break 
- * down the main problem into sub-problems. The sub-problems are recursively solved and 
- * their solutions are added up to produce the full solution to the problem. "Because 
- * divide-and-conquer creates at least two sub-problems, a divide-and-conquer algorithm 
- * makes multiple recursive calls."[1] In Quicksort, all the work happens in the DIVIDE 
- * step and no work happens in the COMBINE step.
+ * The Quick Sort (QS) algorithm is a moderately fast sorting algorithm, but one that 
+ * could take exponential time in the worst case. Nevertheless, it is one of the most 
+ * commonly used sorting algorithms because it is very efficient at sorting large data 
+ * sets. 
  *
  * 
- * ANALYSIS
+ * DIVIDE-AND-CONQUER STRATEGY
+ * 
+ * Quick Sort is a recursive algorithm that uses the Divide-and-Conquer strategy. 
+ * Here is Wikipedia's explanation[1]:
+ * 
+ * 
+ *     In computer science, divide and conquer is an algorithm design paradigm based on 
+ *     multi-branched recursion. A divide-and-conquer algorithm works by recursively 
+ *     breaking down a problem into two or more sub-problems of the same or related type, 
+ *     until these become simple enough to be solved directly. The solutions to the 
+ *     sub-problems are then combined to give a solution to the original problem.
+ * 
+ * 
+ * Divide-and-Conquer usually has three phases: 
+ * 
+ *     * DIVIDE: Carve the problem into sub-problems of the same type. 
+ *     * CONQUER: Recursively solve the smaller problems.  
+ *     * COMBINE: Combine the small chunks into the original data set, which should now
+ *       be problem free. 
+ * 
+ * This strategy can only be applied to problems that can be broken up into smaller
+ * problems of the same or similar type. Therefore, during the Divide phase, the big 
+ * problem is iteratively divided  into smaller chunks, which are then sub-divided into 
+ * even smaller fragments. At this point, the big problem has been reapportioned into 
+ * numerous sub-problems. 
+ * 
+ * Moving on to the Conquer phase, recall that each chunk of the problem has the same 
+ * solution as the original, larger problem. During the second phase, the algorithm is 
+ * applied to each fragment of the original problem. By applying the same process to each 
+ * chunk of the main problem, one by one, you can solve the problem with all of them. 
+ * This leaves you multiple pieces of the original data set, each of which has had their 
+ * problem resolved. 
+ * 
+ * This takes us to the last phase of the Divide-and-Conquer strategy: Combine. In this 
+ * phase, the separated parts of the data set are combined to form the whole again. As 
+ * each chunk is in a "correct" state, the newly combined whole should also have had its 
+ * problem resolved. 
+ * 
+ * 
+ * QUICK SORT EXAMPLE
+ * 
+ * Quick Sort is rather complicated, so here is a short example: 
+ * 
+ * 
+ *  * "Because divide-and-conquer creates at least two sub-problems, a divide-and-conquer
+ * algorithm makes multiple recursive calls."[2] In Quicksort, all the work happens in
+ * the DIVIDE step and no work happens in the COMBINE step.
+ * 
+ *  * to break
+ * down the main problem into sub-problems. Then, the Quick Sort method recursively calls
+ * itself over and over to break these sub-problems into yet smaller sub-problems. Once
+ * the sub-problems have been broken down into their smallest possible size, the process
+ * of solving them starts. Starting with the newest and smallest sub-problems and working
+ * back to the biggest sub-problems, Quick Sort begins to execute the sorting phase of
+ * the algorithm.
  *
  * Quicksort begins by breaking up the starting array into two sub-arrays. Quicksort does
  * a pass over the sub-array's, lightly sorting its contents. After the first pass, each 
@@ -35,15 +83,27 @@
  * each value relative to the pivot. When all elements have been moved to the correct 
  * side of the pivot, the array is said to have been partitioned.
  *
- *
- * "In practice, quicksort outperforms merge sort, and it significantly outperforms 
- * selection sort and insertion sort."[2]
+ * "In practice, quicksort outperforms merge sort, and it significantly outperforms
+ * selection sort and insertion sort."[3]  Quick Sort can sort a data set in O(n log n)
+ * time in the average case (logarithmic time) and O(n^2) in the worst case scenario
+ * (exponential time). 
+ * 
+ * Quick Sort's performance depends largely on Pivot selection. A well-selected pivot 
+ * will let the algorithm execute in logarithmic time, but a poor choice for a pivot may 
+ * result in exponential time. How do you select a pivot? The common choices for a pivot 
+ * are the first, last and middle elements in an array. Under certain circumstances, 
+ * choosing the first or last elements as a pivot could result in worst case performance.
+ * (E.g. If you want to forward sort or reverse sort a list that is already sorted, you
+ * could get exponential time complexity if you choose the first or last elements as the
+ * pivot.) The middle element is usually a better choice, as is the median element (use 
+ * the first, last and middle items to calculate the median element.). However, for large
+ * lists, randomly chosen pivots ensure O(n log n) time complexity.
  *
  *
  * SOURCES
- * 1. Divide and Conquer Algorithms: https://www.khanacademy.org/computing/computer-science/algorithms/merge-sort/a/divide-and-conquer-algorithms
- * 2. Overview of Quicksort: https://www.khanacademy.org/computing/computer-science/algorithms/quick-sort/a/overview-of-quicksort
- *
+ * 1. https://en.wikipedia.org/wiki/Divide-and-conquer_algorithm
+ * 2. Divide and Conquer Algorithms: https://www.khanacademy.org/computing/computer-science/algorithms/merge-sort/a/divide-and-conquer-algorithms
+ * 3. Overview of Quicksort: https://www.khanacademy.org/computing/computer-science/algorithms/quick-sort/a/overview-of-quicksort
  */
 
 
